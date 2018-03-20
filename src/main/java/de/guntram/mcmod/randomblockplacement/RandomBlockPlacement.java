420984fa4a36d59e991d1a36a8bc7c86184092db
Guntram Blohm <gbl@guntram.de>
2018-03-20 13:04:32 +0100

    implement versioncheck

--- src/main/java/de/guntram/mcmod/randomblockplacement/RandomBlockPlacement.java
+++ src/main/java/de/guntram/mcmod/randomblockplacement/RandomBlockPlacement.java
@@ -10,13 +10,14 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
         version = RandomBlockPlacement.VERSION,
 	clientSideOnly = true, 
 	guiFactory = "de.guntram.mcmod.randomblockplacement.GuiFactory",
-	acceptedMinecraftVersions = "[1.12]"
+	acceptedMinecraftVersions = "[1.12]",
+	updateJSON="https://raw.githubusercontent.com/gbl/RandomBlockPlacement/master/versioncheck.json"
 )
 
 public class RandomBlockPlacement
 {
     static final String MODID="randomblockplacement";
-    static final String VERSION="1.0";
+    static final String VERSION="@VERSION@";
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
