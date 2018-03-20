288f12c26391dd0113b0b69a6a268eb9fe463736
Guntram Blohm <gbl@guntram.de>
2018-03-20 12:54:27 +0100

    initial setup

---
+++ src/main/java/de/guntram/mcmod/randomblockplacement/RandomBlockPlacement.java
@@ -0,0 +1,32 @@
+package de.guntram.mcmod.randomblockplacement;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = RandomBlockPlacement.MODID, 
+        version = RandomBlockPlacement.VERSION,
+	clientSideOnly = true, 
+	guiFactory = "de.guntram.mcmod.randomblockplacement.GuiFactory",
+	acceptedMinecraftVersions = "[1.12]"
+)
+
+public class RandomBlockPlacement
+{
+    static final String MODID="randomblockplacement";
+    static final String VERSION="1.0";
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @EventHandler
+    public void preInit(final FMLPreInitializationEvent event) {
+        ConfigurationHandler confHandler = ConfigurationHandler.getInstance();
+        confHandler.load(event.getSuggestedConfigurationFile());
+        MinecraftForge.EVENT_BUS.register(confHandler);
+    }
+}
